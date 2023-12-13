7e1d92b58369b757a4b2ec840fde80114e22f457
Mostafakhalil569 <153729988+Mostafakhalil569@users.noreply.github.com>
2023-12-13 13:47:58 +0200

    Update jira.component.ts

--- src/app/jira/jira.component.ts
+++ src/app/jira/jira.component.ts
@@ -1,9 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrganizationService } from '../organization.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { enviroment } from '../../../enviroment'; 
+
 
 @Component({
   selector: 'app-jira',
@@ -13,25 +10,8 @@ import { enviroment } from '../../../enviroment';
   styleUrl: './jira.component.css'
 })
 export class JiraComponent {
-  constructor(private organization:OrganizationService,private http: HttpClient){}
-  authorization=enviroment.mv_token
-  private allCurstomersurl = 'https://api.xiteit.co/api/customers/?format=json';
-
-
-  getallorganization(): Observable<any> {
-    const headers = new HttpHeaders({
-     'Content-Type': 'application/json',
-     'Authorization': this.authorization,
-   });
-
-   return this.http.get(this.allCurstomersurl, { headers });
- }
+  constructor(){}
   
-  def(){
-   this.getallorganization().subscribe((res) => {
-    console.log(res)
-   })
-  }
 
 
 
